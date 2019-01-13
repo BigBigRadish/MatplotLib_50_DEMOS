90a41fede1b81c245b5eaab0a19e0a15e5924ed4
BigBigRadish <luozhukun@163.com>
2019-01-13 18:58:35 +0800

    线性回归的散点分布图

---
+++ Correlation/Scatter_linear_regression.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 2019年1月13日 下午6:15:50
+Zhukun Luo
+Jiangxi University of Finance and Economics
+'''
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+# Import Data
+df = pd.read_csv("https://raw.githubusercontent.com/selva86/datasets/master/mpg_ggplot2.csv")
+df_select = df.loc[df.cyl.isin([4,8]), :]
+
+# Plot
+sns.set_style("white")
+gridobj = sns.lmplot(x="displ", y="hwy", hue="cyl", data=df_select, 
+                     height=7, aspect=1.6, robust=True, palette='tab10', 
+                     scatter_kws=dict(s=60, linewidths=.7, edgecolors='black'))
+
+# Decorations
+gridobj.set(xlim=(0.5, 7.5), ylim=(0, 50))
+plt.title("Scatterplot with line of best fit grouped by number of cylinders", fontsize=20)
+plt.show()
