b948bc7af27a77f122f511d02e17160810b0210a
BigBigRadish <luozhukun@163.com>
2019-01-14 15:52:56 +0800

    坡度图，最适合比较给定人/项目的“前”和“后”位置

---
+++ Ranking/Slope_Chart.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 2019年1月14日 下午3:46:48
+Zhukun Luo
+Jiangxi University of Finance and Economics
+'''
+'''
+坡度图
+坡度图最适合比较给定人/项目的“前”和“后”位置
+'''
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+import matplotlib.lines as mlines
+# 导入原始数据
+df = pd.read_csv("https://raw.githubusercontent.com/selva86/datasets/master/gdppercap.csv")
+
+left_label = [str(c) + ', '+ str(round(y)) for c, y in zip(df.continent, df['1952'])]
+right_label = [str(c) + ', '+ str(round(y)) for c, y in zip(df.continent, df['1957'])]
+klass = ['red' if (y1-y2) < 0 else 'green' for y1, y2 in zip(df['1952'], df['1957'])]
+
+# draw line
+# 算法来源于https://stackoverflow.com/questions/36470343/how-to-draw-a-line-with-matplotlib/36479941
+def newline(p1, p2, color='black'):
+    ax = plt.gca()
+    l = mlines.Line2D([p1[0],p2[0]], [p1[1],p2[1]], color='red' if p1[1]-p2[1] > 0 else 'green', marker='o', markersize=6)
+    ax.add_line(l)
+    return l
+
+fig, ax = plt.subplots(1,1,figsize=(14,14), dpi= 80)
+
+# 绘制辅助线
+ax.vlines(x=1, ymin=500, ymax=13000, color='black', alpha=0.7, linewidth=1, linestyles='dotted')
+ax.vlines(x=3, ymin=500, ymax=13000, color='black', alpha=0.7, linewidth=1, linestyles='dotted')
+
+# 绘制散点
+ax.scatter(y=df['1952'], x=np.repeat(1, df.shape[0]), s=10, color='black', alpha=0.7)
+ax.scatter(y=df['1957'], x=np.repeat(3, df.shape[0]), s=10, color='black', alpha=0.7)
+
+# 分段注释辅助线
+for p1, p2, c in zip(df['1952'], df['1957'], df['continent']):
+    newline([1,p1], [3,p2])
+    ax.text(1-0.05, p1, c + ', ' + str(round(p1)), horizontalalignment='right', verticalalignment='center', fontdict={'size':14})
+    ax.text(3+0.05, p2, c + ', ' + str(round(p2)), horizontalalignment='left', verticalalignment='center', fontdict={'size':14})
+
+# 'Before' and 'After' 注释
+ax.text(1-0.05, 13000, 'BEFORE', horizontalalignment='right', verticalalignment='center', fontdict={'size':18, 'weight':700})
+ax.text(3+0.05, 13000, 'AFTER', horizontalalignment='left', verticalalignment='center', fontdict={'size':18, 'weight':700})
+
+# 装饰
+ax.set_title("Slopechart: Comparing GDP Per Capita between 1952 vs 1957", fontdict={'size':22})
+ax.set(xlim=(0,4), ylim=(0,14000), ylabel='Mean GDP Per Capita')
+ax.set_xticks([1,3])
+ax.set_xticklabels(["1952", "1957"])
+plt.yticks(np.arange(500, 13000, 2000), fontsize=12)
+
+# 绘画版矩形框使边界线变浅
+plt.gca().spines["top"].set_alpha(.0)
+plt.gca().spines["bottom"].set_alpha(.0)
+plt.gca().spines["right"].set_alpha(.0)
+plt.gca().spines["left"].set_alpha(.0)
+plt.show()
